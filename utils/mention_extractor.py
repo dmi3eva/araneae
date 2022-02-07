7e738a483d0229b625d085a0269df67a4f36b78e
dmi3eva <forkatdmi@gmail.com>
2022-02-07 18:22:56 +0300

    ADD: having

--- utils/mention_extractor.py
+++ utils/mention_extractor.py
@@ -7,6 +7,14 @@ from typing import *
 SCHEMES_PATH = "../datasets/spider/tables.json"
 
 
+class Detail(Enum):
+    MULTI_CONDITIONS = "multi-conditions"
+    ORDER_ASC = "order-asc"
+    ORDER_DESC = "order-desc"
+    MULTI_OPERATIONS = "multi-operations"
+    FROM_VALUE = "from-value"
+
+
 class Subquery(Enum):
     SELECT = "select"
     GROUP_BY = "group_by"
@@ -29,6 +37,7 @@ class ORDER(Enum):
 
 
 AGGREGATIONS = [None, Aggregation.MAX, Aggregation.MIN, Aggregation.COUNT, Aggregation.SUM, Aggregation.AVG]
+CONDITION_OPS = ['not', 'between', '=', '>', '<', '>=', '<=', '!=', 'in', 'like', 'is', 'exists']
 
 
 @dataclass
@@ -78,15 +87,48 @@ class MentionExtractor:
     def parse_val_unit(self, scheme, val_unit, type, input_details, aggregation=0, distinct=None) -> List[Mention]:
         unit_op = val_unit[0]
         col_unit_1 = val_unit[1]
-        mentions = [self.parse_col_unit(scheme, col_unit_1, type, input_details, aggregation, distinct)]
+        val_details = input_details
+        if unit_op:
+            val_details += [Detail.MULTI_OPERATIONS]
+        mentions = [self.parse_col_unit(scheme, col_unit_1, type, val_details, aggregation, distinct)]
         if unit_op:
             col_unit_2 = val_unit[1]
-            from_col_2 = [self.parse_col_unit(scheme, col_unit_2, type, input_details + [unit_op], aggregation, distinct)]
+            from_col_2 = [self.parse_col_unit(scheme, col_unit_2, type, val_details + [unit_op], aggregation, distinct)]
             mentions += from_col_2
         return mentions
 
-    def parse_condition(self, scheme, val_unit, type, input_details, aggregation=0, distinct=None) -> List[Mention]:
+    def parse_value(self, scheme, value, type, details) -> List[Mention]:
+        if isinstance(value, float) or isinstance(value, int) or isinstance(value, str):
+            value_mention = Mention(
+                type=type,
+                db=scheme["db_id"],
+                values=[value]
+            )
+            return [value_mention]
+        details += [Detail.FROM_VALUE]
+        mentions = self.get_mentions_from_sql(scheme['db_id'], value, details=details)
+        return mentions
+
+    def parse_cond_unit(self, scheme, cond_unit, type, input_details) -> List[Mention]:
+        op_id = cond_unit[1]
+        operation = CONDITION_OPS[op_id]
+        val_unit = cond_unit[2]
+        val_1 = cond_unit[3]
+        val_2 = cond_unit[4]
+        details = input_details + [operation]
+        mentions = self.parse_val_unit(scheme, val_unit, type, details)
+        if val_1:
+            mentions = self._add_values_to_mentions(mentions, val_1, scheme, type, details)
+        if val_2:
+            mentions = self._add_values_to_mentions(mentions, val_2, scheme, type, details)
+        return mentions
+
+    def parse_condition(self, scheme, conditions, type, input_details) -> List[Mention]:
         mentions = []
+        if len(conditions) > 1:
+            input_details += [Detail.MULTI_CONDITIONS]
+        for cond_unit in conditions[::2]:
+            mentions += self.parse_cond_unit(scheme, cond_unit, type, input_details)
         return mentions
 
     def extract_from_having(self, scheme, having, details) -> List[Mention]:
@@ -103,9 +145,13 @@ class MentionExtractor:
         if len(order_by) == 0:
             return []
         order = order_by[0]
+        if order == "asc":
+            details += [Detail.ORDER_ASC]
+        elif order == "desc":
+            details += [Detail.ORDER_DESC]
         mentions = []
         for val_unit in order_by[1]:
-            mentions += self.parse_val_unit(scheme, val_unit, Subquery.ORDER_BY, details + [order])
+            mentions += self.parse_val_unit(scheme, val_unit, Subquery.ORDER_BY, details)
         return mentions
 
     def extract_from_select(self, scheme, select, details) -> List[Mention]:
@@ -146,20 +192,31 @@ class MentionExtractor:
         mentions = self.get_mentions_from_sql(db, sql)
         return mentions
 
+    def _add_values_to_mentions(self, mentions, val, scheme, type, details):
+        values_mentions = self.parse_value(scheme, val, type, details)
+        values = []
+        for _v in values_mentions:
+            values += _v.values
+        for _mention in mentions:
+            if not _mention.values:
+                _mention.values = []
+            _mention.values += values
+        return mentions
+
 
 if __name__ == "__main__":
     with open('../datasets/araneae/araneae.json') as table_file:
         araneae = json.load(table_file)
 
     extractor = MentionExtractor()
-    for ind, sample in enumerate(araneae[2:]):
+    for ind, sample in enumerate(araneae[80:]):
         question = sample['question']
         query = sample['query']
         sql = sample['sql']
 
         mentions = extractor.get_mentions_from_sample(sample)
-        if 'group by' in query.lower():
-            a = 7
+        if len(sql['having']) > 0:
+             a = 7
 
 
 
