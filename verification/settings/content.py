d0a847351131d7a52a297f3ee303c7d62c42fc30
forkatdmi@gmail.com <forkatdmi@gmail.com>
2022-04-06 18:15:07 +0300

    ADD: verification, main manual text

--- verification/settings/content.py
+++ verification/settings/content.py
@@ -1,16 +1,60 @@
 INSTRUCTIONS = """
-Доброго времени суток!
+\U0001F600 Здравствуйте! 
 
-<b>Цель:</b> Необходимо проверить, что данный SQL-запрос соответствует вопросу на естественном языке, а сам вопрос корректен. 
- 
-<b>Что может быть не так?</b>
-\U00002776 В SQL могут быть использованы не те сущности базы данных, что упомянуты в запросе.
-\U00002777 SQL может выдавать не тот результат, что ожидается в вопросе.
-\U00002778 Вопрос может звучать не естественно. В нем могут содержаться орфографические, пунктуационные, стилистические ошибки.
-\U00002779 Вопрос может быть не похож на запрос к базе данных.
-\U0000277A Вопрос может выходить за рамки содержащейся в базе данных информации.
-      
-Cправочник по SQL: https://schoolsw3.com/sql/index.php. 
+Мы занимаемся расширением существующего датасета SQL-запросов искусственными методами. Будем очень благодарны, если вы поможете оценить корректность новых примеров.
+<i>Сейчас вы можете пропустить инструкцию и вернуться к ней, когда у вас возникнут вопросы.</i>
+Главное, помните, что, если вы сомневаетесь, то лучше линий раз сообщить об ошибке.
+
+Оценка корректности каждого примера разбита на 4 последовательных шага. 
+
+1️⃣ <b>ШАГ</b>: 
+Звучит ли запрос к базе данных естественно и адекватно?
+
+Примеры:
+\U00002796 What ID has the most surnames? \U00002192 <i>Звучит неестественно. Да и каждому ID, скорее всего, соответствует одно имя. </i>
+\U00002795 Which command has the most wins?
+
+2️⃣ <b>ШАГ</b>: 
+Похож ли вообще данный текст на запрос к БД?
+
+Примеры:
+\U00002796 Any country in the world have five singers. \U00002192 <i>Это просто повествовательное предложение, не запрос.</i>
+\U00002795 List all countries in the world that have five singers.
+
+\U00002796 Why can't you count the number of conductors? \U00002192 <i>Нет запроса на получение информации из БД.</i>
+\U00002795 What the number of conductors?
+
+\U00002796 Compare all companies. \U00002192 <i>Запрос не четкий. Нельзя однозначно перевести его на SQL.</i> 
+\U00002795 Order companies names by their profit margins, please.
+
+\U00002796 All the broadcasting TV Channels don't working today. List the number of different channels by their owners. \U00002192 <i>Многовато лишней информации :)</i>
+\U00002795 List the number of different channels by their owners.
+
+3️⃣ <b>ШАГ</b>: 
+Значат ли одно и то же два данных вопроса?
+
+Примеры:
+\U00002796 
+Give the language that is spoken in the most countries.
+What is the most spoken language in the world?
+\U00002795
+What are the type codes and descriptions for all template types?	
+What are types of templates? What are their descriptions?
+
+4️⃣ <b>ШАГ</b>: 
+Самое сложное: соответствует ли запрос данному SQL?
+
+Примеры:
+\U00002796 
+Show descriptions of simple properties.
+<code>SELECT description FROM Properties WHERE type='Simple'</code>
+\U00002795 
+Show descriptions of simple properties.
+<code>SELECT T2.description FROM Properties AS T1 JOIN Attributes AS T2 ON T1.code  =  T2.code WHERE T1.type='Simple'</code>
+
+Для проверки вы можете заглядывать в таблицы БД.
+Справочник по SQL: https://schoolsw3.com/sql/index.php.
+Если совсем сложно, просто пропускайте. 
 При возникновении вопросов пишите @dmi3eva.
 
 Спасибо за помощь!
