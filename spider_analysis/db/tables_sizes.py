e634cccc46b99572f1c03796188c86d0258314c2
forkatdmi@gmail.com <forkatdmi@gmail.com>
2022-04-26 14:40:25 +0300

    ADD: statistics for Russian

--- spider_analysis/db/tables_sizes.py
+++ spider_analysis/db/tables_sizes.py
@@ -57,13 +57,53 @@ def get_entities_sizes_statistics(connector: SpiderDB) -> Tuple[NumericalStatist
     print(f"Amount of unique values is {len(all_values_list)}")
 
     symbol_sizes = [len(entity) for entity in all_values_list]
-    print(f"Longest value is {all_values_list[symbol_sizes.index(max(symbol_sizes))]}")
+    print(f"Longest value is \"{all_values_list[symbol_sizes.index(max(symbol_sizes))]}\"")
 
     token_sizes = [len(entity.split(' ')) for entity in all_values_list]
-    print(f"Longest value is {all_values_list[token_sizes.index(max(token_sizes))]}")
+    print(f"Longest value is \"{all_values_list[token_sizes.index(max(token_sizes))]}\"")
 
     print(f"Average symbols size if {sum(symbol_sizes) / len(all_values_list)}")
     print(f"Average tokens size if {sum(token_sizes) / len(all_values_list)}")
     symbols_counter = Counter(symbol_sizes)
     tokens_counter = Counter(token_sizes)
     return NumericalStatistics(symbols_counter), NumericalStatistics(tokens_counter)
+
+
+def get_russian_entities_sizes_statistics(connector: SpiderDB) -> Tuple[NumericalStatistics, NumericalStatistics]:
+    """
+    Returns sizes of values in symbols and in tokens
+    """
+    all_sizes = []
+    unique_sizes = []
+    all_values = set()
+    total = len(connector.triples)
+    for ind, _triple in enumerate(connector.triples):
+        if ind % 500 == 0:
+            print(f"{ind} / {total}")
+        db, table, column = _triple
+        values = connector.get_values(db, table, column)
+        unique_values = set(values)
+        all_values = all_values.union(unique_values)
+    all_values_list = [_v for _v in list(all_values) if is_russian(_v)]
+    print(f"Amount of unique values is {len(all_values_list)}")
+
+    symbol_sizes = [len(entity) for entity in all_values_list]
+    print(f"Longest value is \"{all_values_list[symbol_sizes.index(max(symbol_sizes))]}\"")
+
+    token_sizes = [len(entity.split(' ')) for entity in all_values_list]
+    print(f"Longest value is \"{all_values_list[token_sizes.index(max(token_sizes))]}\"")
+
+    print(f"Average symbols size if {sum(symbol_sizes) / len(all_values_list)}")
+    print(f"Average tokens size if {sum(token_sizes) / len(all_values_list)}")
+    symbols_counter = Counter(symbol_sizes)
+    tokens_counter = Counter(token_sizes)
+    return NumericalStatistics(symbols_counter), NumericalStatistics(tokens_counter)
+
+def is_russian(text: str) -> bool:
+    ru_alphabet = 'абвгдеёжзиклмнопрстуфхцчшщъыьэюя'
+    size = len(text)
+    if size == 0:
+        return False
+    if text[0] in ru_alphabet or text[-1] in ru_alphabet or text[size // 2] in ru_alphabet:
+        return True
+    return False
