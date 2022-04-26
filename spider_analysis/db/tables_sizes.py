25ea0f3f4de279fb42172c222c181f4fe846be2d
forkatdmi@gmail.com <forkatdmi@gmail.com>
2022-04-26 13:28:44 +0300

    ADD: get entities sizes statistics

--- spider_analysis/db/tables_sizes.py
+++ spider_analysis/db/tables_sizes.py
@@ -22,6 +22,9 @@ class NumericalStatistics:
 
 
 def get_tables_sizes_statistics(connector: SpiderDB) -> Tuple[NumericalStatistics, NumericalStatistics]:
+    """
+    Returns sizes of tables, amount of unique values in the tables
+    """
     all_sizes = []
     unique_sizes = []
     for _triple in connector.triples:
@@ -33,3 +36,22 @@ def get_tables_sizes_statistics(connector: SpiderDB) -> Tuple[NumericalStatistic
     all_counter = Counter(all_sizes)
     unique_counter = Counter(unique_sizes)
     return NumericalStatistics(all_counter), NumericalStatistics(unique_counter)
+
+
+def get_entities_sizes_statistics(connector: SpiderDB) -> Tuple[NumericalStatistics, NumericalStatistics]:
+    """
+    Returns sizes of values in symbols and in tokens
+    """
+    all_sizes = []
+    unique_sizes = []
+    all_values = set()
+    for _triple in connector.triples:
+        db, table, column = _triple
+        values = connector.get_values(db, table, column)
+        unique_values = set(values)
+        all_values.add(unique_values)
+    symbol_sizes = [len(entity) for entity in list(all_values)]
+    token_sizes = [len(entity.split(' ')) for entity in list(all_values)]
+    symbols_counter = Counter(symbol_sizes)
+    tokens_counter = Counter(token_sizes)
+    return NumericalStatistics(symbols_counter), NumericalStatistics(tokens_counter)
