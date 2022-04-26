af0df0a9122a80aa2a3340e890bf83ae50979acf
forkatdmi@gmail.com <forkatdmi@gmail.com>
2022-04-26 13:55:32 +0300

    FIX: add some statistics

--- spider_analysis/db/tables_sizes.py
+++ spider_analysis/db/tables_sizes.py
@@ -45,13 +45,25 @@ def get_entities_sizes_statistics(connector: SpiderDB) -> Tuple[NumericalStatist
     all_sizes = []
     unique_sizes = []
     all_values = set()
-    for _triple in connector.triples:
+    total = len(connector.triples)
+    for ind, _triple in enumerate(connector.triples):
+        if ind % 500 == 0:
+            print(f"{ind} / {total}")
         db, table, column = _triple
         values = connector.get_values(db, table, column)
         unique_values = set(values)
-        all_values.add(unique_values)
-    symbol_sizes = [len(entity) for entity in list(all_values)]
-    token_sizes = [len(entity.split(' ')) for entity in list(all_values)]
+        all_values = all_values.union(unique_values)
+    all_values_list = list(all_values)
+    print(f"Amount of unique values is {len(all_values_list)}")
+
+    symbol_sizes = [len(entity) for entity in all_values_list]
+    print(f"Longest value is {all_values_list[symbol_sizes.index(max(symbol_sizes))]}")
+
+    token_sizes = [len(entity.split(' ')) for entity in all_values_list]
+    print(f"Longest value is {all_values_list[token_sizes.index(max(token_sizes))]}")
+
+    print(f"Average symbols size if {sum(symbol_sizes) / len(all_values_list)}")
+    print(f"Average tokens size if {sum(token_sizes) / len(all_values_list)}")
     symbols_counter = Counter(symbol_sizes)
     tokens_counter = Counter(token_sizes)
     return NumericalStatistics(symbols_counter), NumericalStatistics(tokens_counter)
