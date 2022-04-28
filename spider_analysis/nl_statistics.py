39814b36fa7190988413b1032216a07a5e82f091
forkatdmi@gmail.com <forkatdmi@gmail.com>
2022-04-28 13:34:51 +0300

    FIX: language choosing in NL analysis

--- spider_analysis/nl_statistics.py
+++ spider_analysis/nl_statistics.py
@@ -7,10 +7,14 @@ from dto.sample import *
 from araneae.wrapper import Araneae
 
 
-def make_nl_frequency_statistics(samples: List[Sample], ignore_agg=True) -> Dict[str, int]:
+def make_nl_frequency_statistics(samples: List[Sample], language: Language, ignore_agg=True) -> Dict[str, int]:
     statistics = {}
     for _sample in samples:
-        for _token in _sample.question_toks:
+        if language is Language.EN:
+            tokens = _sample.question_toks
+        else:
+            tokens = _sample.russian_question_toks
+        for _token in tokens:
             processed = _token.lower()
             statistics[processed] = statistics.get(processed, 0) + 1
     return statistics
@@ -29,6 +33,6 @@ def save_nl_statistics(statistics, filename):
 if __name__ == "__main__":
     araneae = Araneae()
     araneae.load()
-    nl_frequency = make_nl_frequency_statistics(araneae.samples.content, ignore_agg=False)
-    save_nl_statistics(nl_frequency, "09_03_25_nl_frequency")
+    nl_frequency = make_nl_frequency_statistics(araneae.samples.content, Language.EN, ignore_agg=False)
+    save_nl_statistics(nl_frequency, "22_04_28_nl_frequency_en")
     a = 7
